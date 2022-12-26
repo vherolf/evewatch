7acb2ddf3fdd13d0fde0047bb1a3994b8f8448a1
vherolf <vherolf@proclos.com>
2022-12-26 15:19:12 +0100

    added all channels from current day (might need and exclude list) added channel dataclass, so its possible to send beck from which channel the message came

--- evewatch.py
+++ evewatch.py
@@ -10,31 +10,35 @@ import paho.mqtt as mqtt
 start_logging = False
 # changed the first time you jump to another system
 current_solarsystem = ''
+# turn on/off mqtt triggers
+mqtt = True
 
 today = datetime.now()
 chatlogdir = list(Path.home().rglob( 'EVE/logs/Chatlogs' )).pop()
-# chat channel you wanna check
-channellist = ['AKIMA WH','INTEL.RC','Local','Heavy Metal Pirates']
+
 # check for mentions of this usernames
 usernames = ['Vherolf']
 
-# make the chatlognames from today with pathlib glob and the channellist
-chats=[]
-for channel in channellist:
-    chats += list(chatlogdir.glob(channel+'_'+str(today.year) + str(today.month) + str(today.day)+'*'))
-
 # solar systems that should be watched 
 solarsystemlist = ['MVCJ-E','MVC','BK4-YC','2-TEGJ','LF-2KP','F-YH5B','K1I1-J','BK4','BK4-']
+
+@dataclass(frozen=True, slots=True)
+class Channel:
+    channel: str
+    path: Path
+
+chatfiles = chatlogdir.glob( '*'+'_'+str(today.year) + str(today.month) + str(today.day)+'*.txt' )
 
 @dataclass(frozen=True, slots=True)
 class Message:
     timestamp: datetime
     username: str
     message: str
+    channel: str
     #message_hash: str
 
 ## this function is from https://github.com/andrewpmartinez/py-eve-chat-mon (THANK YOU!)
-async def parse_msg(raw_msg) -> Message:
+async def parse_msg(raw_msg, channel) -> Message:
     line_parser = re.compile('^\s*\[\s(.*?)\s\]\s(.*?)\s>\s(.*?)$', re.DOTALL)
     match = line_parser.match(raw_msg)
     if match:
@@ -42,10 +46,12 @@ async def parse_msg(raw_msg) -> Message:
         username = match.group(2)
         message = match.group(3)
         timestamp = datetime.strptime(timestamp, "%Y.%m.%d %H:%M:%S")
-        
+
         parsed_msg = Message(timestamp = timestamp,
                       username = username,
-                      message = message)
+                      message = message,
+                      channel = channel)
+        print(parsed_msg)
         return parsed_msg
 
     return None
@@ -54,18 +60,19 @@ async def parse_msg(raw_msg) -> Message:
 async def system_locator_filter(msg):
     global current_solarsystem
     last_solarsystem = current_solarsystem
-    if msg.username == "EVE System":
-        system_change = msg.message.find('Channel changed to Local :')
-        if system_change != '-1':
-            current_solarsystem = msg.message.split(':')[1].strip()
-            print(f'You just jumped to {current_solarsystem}')
+    
+    system_change = msg.message.find('Channel changed to Local :')
+    if system_change != '-1':
+        current_solarsystem = msg.message.split(':')[1].strip()
+        print(f'Your new location is {current_solarsystem}')
 
 # Filter: fires if someone mention you in a chat
 async def name_filter(msg):
     user = [name for name in usernames if name in msg.message]
     if user:
         print(f"The Pilot {msg.username} wants to talk to you")
-        await mqtttrigger('plug4')
+        if mqtt:
+            await mqtttrigger('plug4')
 
 # Filter: fires if someone is on the proximity list you made
 #          should be automatic one day
@@ -73,7 +80,8 @@ async def proximity_filter(msg):
     solarsystem = [name for name in solarsystemlist if name in msg.message]
     if solarsystem:
         print(f'RUN .. enemy in {solarsystem}')
-        await mqtttrigger('plug3')
+        if mqtt:
+            await mqtttrigger('plug3')
 
 # Trigger: sends a mqtt message (in this case it toggles a plug with a lamp)
 async def mqtttrigger(plug="plug3"):
@@ -85,9 +93,9 @@ async def mqtttrigger(plug="plug3"):
 
 chat_line_delimiter = u"\ufeff"
 # here is all glued together
-async def parselog( log ):
+async def parse_log( chat ):
     # eve log files are utf-16 encoded
-    async with aiofiles.open(log, mode='r',encoding="utf-16-le") as f:
+    async with aiofiles.open(chat.path, mode='r',encoding="utf-16-le") as f:
         # dont like this code much - I needed to add the sleep
         # nicer would be if aiofile would not exit on EOL 
         # and would work without a context manager
@@ -98,19 +106,17 @@ async def parselog( log ):
             await asyncio.sleep(0.01)
             
             if contents and start_logging:
-                msg = await parse_msg(contents)
-                if msg:
-                    print(msg)
-                    # apply filters
-                    match msg.username:
-                        case 'EVE System':
-                            await system_locator_filter(msg)
-                        case 'Message':
-                            pass
-                        case unknown_command:
-                            await proximity_filter(msg)
-                            await name_filter(msg)
-            
+                msg = await parse_msg(raw_msg=contents, channel=chat.channel)
+                # apply filters
+                match msg:
+                    case Message(username='EVE System'):
+                        await system_locator_filter(msg)
+                    case Message(username='Message'):
+                        pass
+                    case _:
+                        await proximity_filter(msg)
+                        await name_filter(msg)
+                
 
 # status report is you want regular reports
 async def status():
@@ -128,9 +134,10 @@ async def startup_log_delay(time=10):
     start_logging = True
 
 async def main():
-    tasks = []        
-    for log in chats:
-        task = asyncio.create_task( parselog(log) )
+    tasks = []
+    for chatfile in chatfiles:
+        print(chatfile)
+        task = asyncio.create_task( parse_log(Channel(chatfile.stem,chatfile)) )
         tasks.append(task)
     
     # add status monitor 
